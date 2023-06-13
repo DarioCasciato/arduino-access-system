308cb358eab34eca09fcb2c7601f49a1cee23eb1
Dario Casciato <93987804+DarioCasciato@users.noreply.github.com>
2023-06-13 17:16:50 +0200

    Added Keypad configurations

--- src/configurations.h
+++ src/configurations.h
@@ -13,6 +13,9 @@
 #define SIGNALIZER_LED          15      ///< LED pin number for the signalizer
 #define SIGNALIZER_OPENER       17      ///< Opener pin number for the signalizer
 
+#define KEYPAD_ROW_NUM          4
+#define KEYPAD_COL_NUM          3
+
 // How long the Lock should be open after authentication in seconds
 #define OPEN_TIME               3       ///< Open time duration in seconds
 
@@ -25,5 +28,16 @@
 #define ADDRESS_WHITELIST       0x00F   ///< Address of the whitelist in EEPROM
 #define ADDRESS_WHITELISTCOUNT  0x000   ///< Address of the whitelist count value in EEPROM
 #define ADDRESS_MASTER          0x006   ///< Address of the master value in EEPROM
+
+// Keypad configurations
+char keys[KEYPAD_ROW_NUM][KEYPAD_COL_NUM] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+
+unsigned char keypad_pin_rows[KEYPAD_ROW_NUM] = {8, 3, 4, 6};
+unsigned char keypad_pin_column[KEYPAD_COL_NUM] = {7, 9, 5};
 
 #endif // ARDUINO_ACCESS_SYSTEM_CONFIGURATIONS_
